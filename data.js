fc436c5ff1743677b2ab0b779d1b3a2143c66d5b
Biruk Mugoro <birukmugoromg@gmail.com>
2023-12-31 16:07:46 +0300

    music player commit

---
+++ data.js
@@ -0,0 +1,39 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'musics/entone.mp3',
+        artist: 'artist 1',
+        cover: 'images/entone.png'
+    },
+    {
+        name: 'song 2',
+        path: 'musics/entone1.mp3',
+        artist: 'artist 2',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'musics/entone2.mp3',
+        artist: 'artist 3',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'musics/entone3.mp3',
+        artist: 'artist 4',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'musics/entone3.mp3',
+        artist: 'artist 5',
+        cover: 'images/entone3'
+    },
+    {
+        name: 'song 6',
+        path: 'musics/entone3.mp3',
+        artist: 'artist 6',
+        cover: 'images/entone3'
+    }
+]
+
